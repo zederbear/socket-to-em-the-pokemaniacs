5303d2e903a296a89e6a4225115f6aed8180a42e
Koen Kleifgen <Corn.Bunny96@gmail.com>
2025-02-18 15:07:11 -0700

    start of tag players

--- game.py
+++ game.py
@@ -7,3 +7,16 @@ def map_display():
 class map():
   pass
 
+class player():
+  def __init__(self, x, y, type):
+    self.x = x
+    self.y = y
+    self.type = type
+    self.id = players.length
+
+  def move(self, x, y):
+    self.x = x
+    self.y = y
+
+  def tag(self):
+    
