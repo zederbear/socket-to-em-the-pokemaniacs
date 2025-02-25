d4d42a74f1032a1305959f48b2da91aa8d355468
Cameron <magicat8bil@gmail.com>
2025-02-25 14:18:23 -0700

    save

--- game.py
+++ game.py
@@ -22,8 +22,9 @@ class Game:
         for y in range(len(self.game_map)):
             for x in range(len(self.game_map[y])):
                 if self.game_map[y][x] == 0: validPos.append([x, y])
+                print(self.game_map[y][x], end = '')
+            print()
         pos = validPos[random.randint(0, len(validPos) - 1)]
-        pos = validPos[1]
         return float(pos[0]), float(pos[1])
 
     def display_map(self):
